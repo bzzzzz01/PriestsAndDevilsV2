ec2ab3a25697370a4b5b6192ea3b9fe177a98e47
bzzzzz01 <905161357@qq.com>
2018-04-10 23:42:24 +0800

    Add files via upload

---
+++ homework3/MoveToAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToAction : Action {
+    public Vector3 target;
+    public float speed;
+
+    public static MoveToAction GetAction(Vector3 target, float speed)
+    {
+        MoveToAction action = CreateInstance<MoveToAction>();
+        action.target = target;
+        action.speed = speed;
+        return action;
+    }
+
+    public override void Start() { }
+
+    public override void Update()
+    {
+        this.GameObject.transform.position = Vector3.MoveTowards(this.GameObject.transform.position, target, speed * Time.deltaTime);
+        if (this.GameObject.transform.position == target)
+        {
+            this.destroy = true;
+            this.Callback.ActionEvent(this);
+        }
+    }
+}
